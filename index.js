5f34aa98b34a8ee1ee2b7be2b7bcea4c016f01a1
Semaphore <kristen.m.eversole@gmail.com>
2021-09-29 15:14:12 -0500

    Opacity changing and media queries added

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
 let container = document.querySelector('#container');
 
+function createDivs() {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.addEventListener('mouseover', changeColor)
+    container.appendChild(pixel);
+}
+
+function clearGrid() {
+    pixelArray = document.getElementsByClassName("pixel");
+    for (i=0; i < pixelArray.length; i++) {
+        pixelArray[i].style.opacity = 0;
+    }
+}
+
 function emptyGrid(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-function changeColor(e) {
-    e.target.style.backgroundColor = "black";
+function startingGrid(num) {
+    for (i = 0; i < (num * num); i++) {
+        createDivs();
+        container.style.gridTemplateColumns = `repeat(${num}, 1fr)`
+        container.style.gridTemplateRows = `repeat(${num}, 1fr)`
+    }
 }
 
+function changeColor(e) {
+    let currentOpacity = window.getComputedStyle(e.target).getPropertyValue("opacity");
+        e.target.style.opacity = Number(currentOpacity) + .1; 
+}
+
+
+
 function createGrid() {
-    let number = parseInt(document.getElementById('numberInput').value);
-    let numberSquared = Math.pow(number, 2);
+    let number = parseInt(prompt("Enter a number between 1 and 100"));
+    let numberSquared = number * number;
     if ((isNaN(number)) || (number === "") || (typeof(number) !==  'number')) {
         alert("Invalid entry!")
     } else if ((number > 100) || (number < 0)) {
         alert("Try a number between 1 and 100.");
     } else emptyGrid(container);
     for (i = 0; i < numberSquared; i++) {
-        let pixel = document.createElement("div");
-        pixel.addEventListener('mouseover', changeColor)
-        container.appendChild(pixel);
+        createDivs();
         container.style.gridTemplateColumns = `repeat(${number}, 1fr)`
         container.style.gridTemplateRows = `repeat(${number}, 1fr)`
     }
     }
 
-createGrid(16);
+startingGrid(16);
 
-document.getElementById("button").addEventListener("click", createGrid, false)
+document.getElementById("clear").addEventListener("click", clearGrid, false)
+document.getElementById("newCanvas").addEventListener("click", createGrid, false)
